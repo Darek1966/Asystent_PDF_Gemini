edf3efc5e491f2742c75576f2d3d6ef629448dbb
Darek1966 <dar.ch983@gmail.com>
2025-08-10 11:28:22 +0200

    Usunięcie pliku test_api.py

--- test_api.py
+++
@@ -1,8 +0,0 @@
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-result = embeddings.embed_documents(["Test embedding", "Drugi testowy dokument"])
-print(result)
